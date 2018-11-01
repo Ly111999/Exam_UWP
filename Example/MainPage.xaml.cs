c4361151b0be189d41b430cbb957b044c803e853
Ly111999 <39553327+Ly111999@users.noreply.github.com>
2018-11-01 09:56:21 +0700

    get data suceess

--- Example/MainPage.xaml.cs
+++ Example/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -12,6 +13,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Example.Model;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,9 +24,25 @@ namespace Example
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        public static ObservableCollection<POJO.RootObject> list { get; set; }
+
         public MainPage()
         {
             this.InitializeComponent();
+            list = new ObservableCollection<POJO.RootObject>();
+            this.DataContext = this;
+
+        }
+
+        private async void MainPage_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            List<POJO.RootObject> dateYoutube = await POJO.GetYoutube();
+            for (int i = 0; i < dateYoutube.Count; i++)
+            {
+                list.Add(dateYoutube[i]);
+            }
+
+            GridViewShow.ItemsSource = list;
         }
     }
 }
---
+++ Example/Model/POJO.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Example.Model
+{
+    public class POJO
+    {
+        public static async Task<List<POJO.RootObject>> GetYoutube()
+        {
+            var http = new HttpClient();
+            var url =
+                "http://youtube-video-api-1608.appspot.com/youtube/api";
+            var resp = await http.GetAsync(url);
+            var result = await resp.Content.ReadAsStringAsync();
+            List<POJO.RootObject> list = JsonConvert.DeserializeObject<List<POJO.RootObject>>(result);
+            return list;
+        }
+
+        public class RootObject
+        {
+            public string videoId { get; set; }
+            public string name { get; set; }
+            public string description { get; set; }
+            public string keywords { get; set; }
+            public string category { get; set; }
+            public string genre { get; set; }
+            public string authorName { get; set; }
+            public string authorEmail { get; set; }
+            public string birthday { get; set; }
+        }
+    }
+}
